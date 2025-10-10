01a1816450019c13bc276df6db9190bd039dea43
MARTIN MONDINO <tincho_mondino@hotmail.com>
2025-10-10 14:33:30 +0200

    llm_with_structured_output example

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
-
-from langchain import hub
-from langchain.agents.react.agent import create_react_agent
-from langchain.agents import AgentExecutor
 
 load_dotenv()
 
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
 
 def main():
-    print("Running Create React Agent")
+    tools = [TavilySearch()]
+    llm = ChatOpenAI(model="gpt-4.1-nano")
+    structured_llm = llm.with_structured_output(AgentResponse)
+    # react_prompt = hub.pull("hwchase17/react")
 
-    llm_model = ChatOpenAI(model="gpt-4.1-nano", temperature=0.25)
+    react_prompt_with_format_instructions = PromptTemplate(
+        template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+        input_variables=["input", "agent_scratchpad", "tool_names"],
+    ).partial(format_instructions="")
 
-    react_prompt = hub.pull("hwchase17/react")
-
-    search_tool = TavilySearch(max_results=2)
-
-    # llm_model_with_tools = llm_model.bind_tools([search_tool])
-
-    react_agent = create_react_agent(llm=llm_model, tools=[search_tool], prompt=react_prompt)
-
-    agent_executor = AgentExecutor(agent=react_agent, tools=[search_tool], verbose=True)
-
-    result = agent_executor.invoke(
-        {"input": "How much money did US swapped with Argentina yesterday?"}
+    agent = create_react_agent(
+        llm=llm,
+        tools=tools,
+        prompt=react_prompt_with_format_instructions,
     )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    extract_output = RunnableLambda(lambda x: x["output"])
+    chain = agent_executor | extract_output | structured_llm
 
+    result = chain.invoke(
+        input={
+            "input": "Tell me the last results of Inter Miami soccer team",
+        }
+    )
     print(result)
 
 
